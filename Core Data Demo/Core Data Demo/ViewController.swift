3ad280296ef5fe127e2fe637139b37e6a9634f5f
dandyxu <dandyjefferson@gmail.com>
2015-09-19 14:37:01 +0100

    Add & Delete & Update & Select (Search) of database

--- Core Data Demo/Core Data Demo/ViewController.swift
+++ Core Data Demo/Core Data Demo/ViewController.swift
@@ -19,27 +19,53 @@ class ViewController: UIViewController {
         
         var context:NSManagedObjectContext = AppDel.managedObjectContext!
     
-        var newUser = NSEntityDescription.insertNewObjectForEntityForName("User", inManagedObjectContext: context) as! NSManagedObject
-        
-        newUser.setValue("Dandy", forKey: "username")
-        newUser.setValue("Pass", forKey: "password")
-        newUser.setValue("dandyjefferson@gmail.com", forKey: "email")
-        
-        context.save(nil)
+        /* Add specfic item into database */
+//        var newUser = NSEntityDescription.insertNewObjectForEntityForName("User", inManagedObjectContext: context) as! NSManagedObject
+//
+//        newUser.setValue("DandyJefferson3", forKey: "username")
+//        newUser.setValue("Pass3", forKey: "password")
+//        newUser.setValue("dandyjefferson@gmail.com", forKey: "email")
+//        context.save(nil)
         
         var request = NSFetchRequest(entityName: "User")
         
         request.returnsObjectsAsFaults = false
         
+        //Search for the specfic item
+        request.predicate = NSPredicate(format: "username = %@", "DandyJefferson")
+        
         var results = context.executeFetchRequest(request, error: nil)
         
-        println(results)
+        //println(results)
         
         if results?.count > 0{
         
             for result: AnyObject in results!{
             
-            println(result.valueForKey("username")!)
+                //println(results)
+//                if let pass = result.password!{
+//                    //println(pass)
+//                }
+                
+                if let username = result.valueForKey("username") as? String{
+                    //println(user)
+                    if username == "DandyJefferson" {
+                        // Delete specfic item
+//                        context.deleteObject(result as! NSManagedObject)
+//                        println(username + " has been deleted")
+                     //println(username)
+                        //Update spefic item in the database
+                        result.setValue("Dandy", forKey: "username")
+                    }
+
+                }
+                println(result.valueForKey("username") as! String)
+                println(result.valueForKey("password") as! String)
+                println(result.valueForKey("email") as! String)
+                
+                context.save(nil)
+                
+                    //println(result.valueForKey("username")!)
             
             }
         }else {
