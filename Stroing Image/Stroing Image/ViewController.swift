f306b9cdbe568e3bcf4cec14f470c17ef926900f
dandyxu <dandyjefferson@gmail.com>
2015-09-22 20:49:46 +0100

    Download image online and save it locally and access it without connection

--- Stroing Image/Stroing Image/ViewController.swift
+++ Stroing Image/Stroing Image/ViewController.swift
@@ -10,9 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bach: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlPath = "http://i.zeze.com/attachment/forum/201504/14/232615q17d1t77xshibl17.png"
+        let url = NSURL(string:urlPath)
+        
+        let urlRequest = NSURLRequest(URL:url!)
+        
+        //Multithreading in asynchronous
+        NSURLConnection.sendAsynchronousRequest(urlRequest, queue: NSOperationQueue.mainQueue(), completionHandler: {
+            (response,data,error) in
+            
+            if error != nil {
+                println("There was an error!")
+                
+            } else {
+                
+                let image = UIImage(data:data)
+                
+                //self.bach.image = image
+                
+                //Save online image and access image without Internet connection
+                var DocumentDirectory:String?
+                
+                var paths:[AnyObject] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+                
+                if paths.count > 0 {
+                    DocumentDirectory = paths[0] as? String
+                    
+                    var savePath = DocumentDirectory! + "/bach.jpg"
+                    
+                    NSFileManager.defaultManager().createFileAtPath(savePath, contents: data, attributes: nil)
+                    
+                    self.bach.image = UIImage (named: savePath)
+                    
+                }
+                
+                
+            }
+            
+            
+        })
+        
+        
         
         
     }
